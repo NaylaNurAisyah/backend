ef17133b98b9c1faa5b63c40c84287807be85b68
NaylaNurAisyah <98145666+NaylaNurAisyah@users.noreply.github.com>
2025-07-08 23:26:33 +0700

    Update users.js

--- api/users.js
+++ api/users.js
@@ -32,6 +32,9 @@ export default async function handler(req, res) {
   }
 
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method === "GET") {
     const users = await col.find().toArray();
     return res.status(200).json(users);
