5b499dcd2dff3198743801d3eeaf6d1c9ce1cb61
NaylaNurAisyah <98145666+NaylaNurAisyah@users.noreply.github.com>
2025-07-08 15:45:49 +0700

    Update avatar.js

--- api/avatar.js
+++ api/avatar.js
@@ -1,10 +1,8 @@
 export default async function handler(req, res) {
-  // CORS setup
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Ganti "*" dengan "https://vinzzyy.my.id" jika ingin lebih aman
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // OPTIONS preflight
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
