dba48a811345c1941a5d8ff03843e2b2ecc95a24
NaylaNurAisyah <98145666+NaylaNurAisyah@users.noreply.github.com>
2025-07-09 07:44:10 +0700

    Update users.js

--- api/users.js
+++ api/users.js
@@ -5,11 +5,13 @@ const client = new MongoClient(uri);
 const dbName = 'ssr_db';
 
 export default async function handler(req, res) {
+  // Tambah header CORS untuk semua jenis request
+  res.setHeader('Access-Control-Allow-Origin', 'https://vinzzyy.my.id');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   // Handle preflight request
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.status(200).end();
   }
 
@@ -24,9 +26,6 @@ export default async function handler(req, res) {
 
     const db = client.db(dbName);
     const collection = db.collection("users");
-
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === "GET") {
       const users = await collection.find().toArray();
