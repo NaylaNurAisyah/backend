24ac25d042cc04c1e082cfca2ce2167245804e06
NaylaNurAisyah <98145666+NaylaNurAisyah@users.noreply.github.com>
2025-07-19 22:51:21 +0700

    Update [id].js

--- api/robloxinfo/[id].js
+++ api/robloxinfo/[id].js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const id = req.url.split("/").pop(); // manual ambil id
 
   // CORS headers
