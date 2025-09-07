de0f98bed28cd449bd3a1993190935c5f365dad1
NaylaNurAisyah <98145666+NaylaNurAisyah@users.noreply.github.com>
2025-09-07 12:06:59 +0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -8,9 +8,14 @@ const dbName = "Vinzzyy";
 const usersCollection = client.db(dbName).collection("users");
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight OPTIONS
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   await client.connect();
 
   if (req.method === "POST" && req.query.action === "register") {
